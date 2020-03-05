0be367ad2950892bf08e3f47617ca3fbcf989f56
nakanishi-ryotaro <setthesunset25002000@gmail.com>
2020-03-05 14:37:50 +0900

    Pinball 3Dゲーム
    unityの練習で作ったピンボールのゲーム

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+    private GameObject scoreText;
+    private int score=0;
+	// Use this for initialization
+	void Start () {
+        this.scoreText = GameObject.Find("ScoreText");
+        SetScore();
+    }
+	// Update is called once per frame
+	void OnCollisionEnter (Collision collision)
+    {
+        string yourTag = collision.gameObject.tag;
+        if (yourTag == "SmallStarTag")
+        {
+            score += 1;
+        }
+        else if (yourTag == "LargeStarTag")
+        {
+            score += 3;
+        }
+        else if (yourTag == "SmallCloudTag")
+        {
+            score += 5;
+        }
+        else if (yourTag == "LargeCloudTag")
+        {
+            score += 10;
+        }
+        SetScore();
+    }
+    void SetScore()
+    {
+        this.scoreText.GetComponent<Text>().text = score.ToString();
+    }
+}
